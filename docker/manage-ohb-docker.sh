617d507b5577c6b5247f3c373432d7569b57efc4
Dave Koberstein <davek@komacke.com>
2026-05-15 19:46:10 -0700

    Bump to new pskr release

--- docker/manage-ohb-docker.sh
+++ docker/manage-ohb-docker.sh
@@ -19,7 +19,7 @@
 OHB_MANAGER_VERSION=latest
 # tags to use
 VOACAP_SERVICE_TAG=1.15
-PSKR_MQTT_CACHE_TAG=1.12
+PSKR_MQTT_CACHE_TAG=1.13
 
 GITHUB_LATEST_RELEASE_URL="https://api.github.com/repos/komacke/open-hamclock-backend/releases/latest"
 OHB_HTDOCS_DVC=ohb-htdocs
