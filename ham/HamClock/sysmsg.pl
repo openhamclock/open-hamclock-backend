3a02d6b6b51de7663adb7ef3a51423ab7001f9ab
Dave Koberstein <davek@komacke.com>
2026-05-25 13:40:43 -0700

    Initial endpoint for sys messages
    Beginning support for backend sys messages to hamclocks

---
+++ ham/HamClock/sysmsg.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+# Copyright (C) 2026 Open HamClock Backend (OHB) Contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+use CGI;
+
+my $cache_dir = "/opt/hamclock-backend/cache";
+my $content = <<EOF;
+This is a test message. Normally empty or blank.
+EOF
+
+my $q = CGI->new;
+print $q->header('text/plain');
+print $content;
